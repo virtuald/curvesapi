5966b477460f15aec67747a41c9cb451364f5264
Dustin Spicuzza <dustin@virtualroadside.com>
2015-02-21 13:11:31 -0500

    Remove old build files

--- src/makeapi.bat
+++
@@ -1,1 +0,0 @@
-javadoc -d ..\api com\graphbuilder\struc\*.java com\graphbuilder\math\*.java com\graphbuilder\math\func\*.java com\graphbuilder\geom\*.java com\graphbuilder\curve\*.java
--- src/makejar.bat
+++
@@ -1,7 +0,0 @@
-javac com\graphbuilder\struc\*.java
-javac com\graphbuilder\math\func\*.java
-javac com\graphbuilder\math\*.java
-javac com\graphbuilder\geom\*.java
-javac com\graphbuilder\curve\*.java
-
-jar cfv Capi.jar com\graphbuilder\struc\*.class com\graphbuilder\math\func\*.class com\graphbuilder\math\*.class com\graphbuilder\geom\*.class com\graphbuilder\curve\*.class license.txt
