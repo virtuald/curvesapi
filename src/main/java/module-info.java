971484420722139348836319fab0af512e616d21
Adi <adrianionlazar@gmail.com>
2020-01-23 15:39:36 +0100

    added module-info for compatibility with OpenJDK;

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module curvesapi {
+    requires transitive java.desktop;
+
+    exports com.graphbuilder.curve;
+    exports com.graphbuilder.geom;
+    exports com.graphbuilder.math;
+    exports com.graphbuilder.math.func;
+    exports com.graphbuilder.org.apache.harmony.awt.gl;
+    exports com.graphbuilder.struc;
+}
